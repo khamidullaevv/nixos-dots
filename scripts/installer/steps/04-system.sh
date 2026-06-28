90cb24aace101448bea0f6b1399c6728d545dbc2
khamidullaevv <yusufbek090110@gmail.com>
2026-06-28 22:05:04 +0500

    refactor(steps/04-system.sh): edit new config

--- scripts/installer/steps/04-system.sh
+++ scripts/installer/steps/04-system.sh
@@ -5,4 +5,15 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 source "$SCRIPT_DIR/lib.sh"
-echo "Generating system configuration..."
+
+section "Generating Configuration"
+
+source "$SCRIPT_DIR/generators/configuration.sh"
+source "$SCRIPT_DIR/generators/home.sh"
+source "$SCRIPT_DIR/generators/hardware.sh"
+
+generate_configuration
+generate_home
+generate_hardware
+
+success "Configuration generated"
