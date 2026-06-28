e3a2c9f81e820a58c232d8aac9afc00605da0368
khamidullaevv <yusufbek090110@gmail.com>
2026-06-28 22:21:14 +0500

    create(/templates/deafult.nix.in): create deafult.nix.in config

---
+++ hosts/nix-os/configuration.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, inputs, unstable, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+
+    ../../modules/boot
+    ../../modules/networking
+    ../../modules/nix
+
+    ../../modules/users
+
+    ../../modules/services
+
+    ../../modules/hardware
+
+    ../../modules/security
+
+    ../../modules/system
+  ];
+
+  networking.hostName = "nix-os";
+
+  time.timeZone = "Asia/Tashkent";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  system.stateVersion = "26.05";
+}
