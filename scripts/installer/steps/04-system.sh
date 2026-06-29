0864f4bf1d3c6b8bf99d61473bb9a4d7d6a6841e
khamidullaevv <yusufbek090110@gmail.com>
2026-06-29 23:10:21 +0500

    edit(steps/system.sh): edit config system.sh

--- scripts/installer/steps/04-system.sh
+++ scripts/installer/steps/04-system.sh
@@ -5,6 +5,7 @@ set -Eeuo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 source "$SCRIPT_DIR/lib.sh"
+
 source "$GENERATOR_DIR/configuration.sh"
 source "$GENERATOR_DIR/home.sh"
 source "$GENERATOR_DIR/hardware.sh"
