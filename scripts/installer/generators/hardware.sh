cd2f1d62599f3d889d8e04107fc912a39384510b
khamidullaevv <yusufbek090110@gmail.com>
2026-06-29 23:09:38 +0500

    refactor(generators/hardware):edit config hardware.sh

--- scripts/installer/generators/hardware.sh
+++ scripts/installer/generators/hardware.sh
@@ -4,27 +4,20 @@ set -Eeuo pipefail
 
 generate_hardware() {
 
-    [[ -f "$STATE_DIR/config.env" ]] || error "config.env not found."
+    [[ -f "$STATE_DIR/config.env" ]] \
+        || error "config.env not found."
 
     source "$STATE_DIR/config.env"
 
     local HOST_DIR="$ROOT_DIR/hosts/$HOSTNAME"
-    local OUTPUT="$OUTPUT_DIR/hardware"
 
     ensure_dir "$HOST_DIR"
 
-    rm -rf "$OUTPUT"
-    mkdir -p "$OUTPUT"
-
-    info "Generating hardware configuration..."
+    info "Generating hardware-configuration.nix"
 
     sudo nixos-generate-config \
         --show-hardware-config \
-        > "$OUTPUT/hardware-configuration.nix"
-
-    cp \
-        "$OUTPUT/hardware-configuration.nix" \
-        "$HOST_DIR/hardware-configuration.nix"
+        > "$HOST_DIR/hardware-configuration.nix"
 
     success "Hardware configuration generated"
     info "File: $HOST_DIR/hardware-configuration.nix"
